fa13d64e72cc915dae7f1b51c1aa964b4eda6365
Vitor <vitordrager09@gmail.com>
2023-05-30 18:59:58 -0300

    Criação da api com modelo basico e uma rota basica para teste

---
+++ app/controllers/tutorial.controller.js
@@ -0,0 +1,38 @@
+const db = require("../models")
+const Tutorial = db.tutorials;
+const Op = db.Sequelize.Op;
+
+//CRIAR E SALVAR UM NOVO TUTORIAL
+exports.create = (req,res) => {
+
+}
+
+//RETORNAR TODOS OS TUTORIAIS DO BANCO DE DADOS
+exports.findAll = (req, res) => {
+
+}
+
+//RETORNAR POR ID
+exports.findOne = (req,res) => {
+
+}
+
+//ATUALIZAR UM TUTORIAL POR ID
+exports.update = (req,res) => {
+
+}
+
+//DELETAR POR ID
+exports.delete = (req,res) => {
+
+}
+
+//DELETAR TODOS OS TUTORIAIS
+exports.deleteAll = (req,res) => {
+
+}
+
+//RETORNAR TODOS OS TUTORIAIS PUBLICADOS
+exports.findAllPublished = (req,res) => {
+
+}
