581ee864684822a6cbcb52f072e67fe264b10dc8
Vitor <vitordrager09@gmail.com>
2023-05-31 00:58:03 -0300

    corrigido e funcionando

--- app/models/tutorial.model.js
+++ app/models/tutorial.model.js
@@ -1,17 +1,18 @@
-const { sequelize, Sequelize } = require(".");
+
 //AQUI CRIAMOS UMA TABELA NO BANCO POR MEIO DO SEQUELIZE
 //O SEQUELIZE TUTORIAL.MODEL.JS REPRESENTA UMA TABELA NO MYSQL
 module.exports = (sequelize, Sequelize) => {
     const Tutorial = sequelize.define("tutorial", {
         title: {
-            type: Sequelize.STRING
+        type: Sequelize.STRING
         },
         description: {
-            type: Sequelize.STRING
+        type: Sequelize.STRING
         },
         published: {
-            type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN
         }
-    })
-    return Tutorial
-}
+    });
+
+    return Tutorial;
+  };
--- server.js
+++ server.js
@@ -12,19 +12,35 @@ app.use(cors(corsOptions))
 app.use(express.json())
 
 app.use(express.urlencoded({ extend: true }))
-
-// db.sequelize.sync()
-db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.");
-  });
+//sincronizar o banco
+db.sequelize.sync()
+.then(() => {
+    console.log("Synced db.");
+})
+.catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+});
+// reiniar o banco!
+// db.sequelize.sync({ force: true }).then(() => {
+//         console.log("Drop and re-sync db.");
+//       });
+    
+    
+    
 //rota
 //DEFINIDA A PRIMEIRA ROTA
+
+
+
 app.get("/", (req,res) => {
     res.json({message: "BEM VINDO A DRAGER API."})
 })
 
+require("./app/routes/tutorial.routes")(app);
 //CONFIGURADO A PORTA E O LISTEN
 const PORT = process.env.PORT || 8080;
+
+
 
 app.listen(PORT, () => {
     console.log (`SERVIDOR RODANDO NA PORTA ${PORT}.`)
